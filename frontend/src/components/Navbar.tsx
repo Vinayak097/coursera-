ad4fc56f24db761a86e55c417d197921b8fad463
unknown <vinayak20injamure@gmail.com>
2024-07-23 21:16:32 +0530

    backend done

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -6,20 +6,22 @@ import { IoMenuOutline } from "react-icons/io5";
 function Navbar() {
   const [sideon,setsideon]=useState(false);
   return (
-    <div className='bg-blue-100 p-2 flex flex-row justify-between transition-all items-center px-10 '>
-      <div onClick={()=>{setsideon(sideon?false:true)}} className='p-1 text-3xl transition-all bg-gradient-to-br from-blue-400 via-purple-500 rounded-md to-voilet-500'>
+    <div className=' shadow-lg  p-2 flex flex-row justify-between transition-all items-center px-10 '>
+
+      <div onClick={()=>{setsideon(sideon?false:true)}} className='p-1 text-3xl text-secondary transition-all text-gradient-to-br from-blue-400 via-purple-500 rounded-md to-voilet-500'>
         {sideon?  <MdCancel className=''/> :<IoMenuOutline></IoMenuOutline>}
       
      
       </div>
-        <div className='flex  text-white '>
-            <MdViewInAr className='text-blue text-[40px]  rounded-md bg-gradient-to-r from-violet-500 via-purple-500 to-purple-800'/>
+        <div className='flex  text-secondary '>
+            <MdViewInAr className='text-blue text-[40px] p-2  rounded-md bg-gradient-to-r from-violet-500 via-purple-500 to-purple-800'/>
             <p className=' flex text-xl'><span className='text-violet-500'>g</span> <span className='text-amber-500'>te</span> <span className='text-blue-950'>x</span></p>
         </div>
          <div>
-            <FaUserCircle className='text-3xl rounded-full text-white bg-gradient-to-r from-violet-500 via-purple-500 to-purple-800' />
+            <FaUserCircle className='text-3xl rounded-full text-neutral bg-gradient-to-r from-violet-500 via-purple-500 to-purple-800' />
          </div>
     </div>
+
   )
 }
 
--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -4,15 +4,12 @@ import { useRecoilValue } from 'recoil'
 function Sidebar() {
     const sideSelected=useRecoilValue(sideSelect);    
   return (
-    <div className='bg-violet-400'>
-        <div className='h-screen  text-stone-800 text-xl flex flex-col items-center gap-2 '>
+    <div className='  bg-neutral dark:text-white'>
+        <div className='h-screen  text-xl flex flex-col items-center gap-2 '>
             <h2 className='shadow p-2 mt-10'>Courses</h2>
             <h2 className='p-2 shadow'>purchases</h2>
             <h2 className='p-2 shadow'>History</h2>
-            
-
         </div>
-        
     </div>
   )
 }
